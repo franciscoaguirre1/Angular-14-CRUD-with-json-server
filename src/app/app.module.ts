0050632fa3882892de52da273af1ff31b9a96dd7
Francisco Aguirre <franmaguirre@gmail.com>
2022-11-11 06:55:25 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeDashboardComponent  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
