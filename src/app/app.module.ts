95b6e3e6cf2a98fa15e4663af7e28b30eb378cbe
Francisco Aguirre <franmaguirre@gmail.com>
2022-11-13 11:12:09 -0300

    login and signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeDashboardComponent  ],
+    EmployeeDashboardComponent,
+    LoginComponent,
+    SignupComponent  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
